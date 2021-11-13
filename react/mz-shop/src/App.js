2849f940b6e384fdcde8a9076be880975e9344ae
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2021-11-13 22:24:22 +0200

    React => convert to jsx

--- react/mz-shop/src/App.js
+++ react/mz-shop/src/App.js
@@ -1,32 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import Product from './Product'
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement(
-      'h1',
-      /** the class is a reserved keyword in JavaScript and since we use JSX in React which itself is the extension of JavaScript, so we have to use className instead of class attribute. */
-      { id: 'my-brand', className: 'logo' },
-      'MZ Shop!'
-    ),
-    // Three stamp of that product component
-    React.createElement(Product, {
-      title: 'Product 1',
-      description: 'Product 1 Desc',
-      price: '10$'
-    }),
-    React.createElement(Product, {
-      title: 'Product 2',
-      description: 'Product 2 Desc',
-      price: '20$'
-    }),
-    React.createElement(Product, {
-      title: 'Product 3',
-      description: 'Product 3 Desc',
-      price: '30$'
-    })
-  ])
+  return (
+    <div>
+      <h1 id="my-brand" className="logo">
+        MZ Shop!
+      </h1>
+      <Product title="Product 1" description="Product 1 Desc" price="10$" />
+      <Product title="Product 2" description="Product 2 Desc" price="20$" />
+      <Product title="Product 3" description="Product 2 Desc" price="30$" />
+    </div>
+  )
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+render(<App />, document.getElementById('root'))
--- react/mz-shop/src/Product.js
+++ react/mz-shop/src/Product.js
@@ -1,11 +1,11 @@
-import React from 'react'
-
 const Product = (props) => {
-  return React.createElement('div', {}, [
-    React.createElement('h2', {}, props.title),
-    React.createElement('h3', {}, props.description),
-    React.createElement('h3', {}, props.price)
-  ])
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.description}</h2>
+      <h2>{props.price}</h2>
+    </div>
+  )
 }
 
 export default Product
