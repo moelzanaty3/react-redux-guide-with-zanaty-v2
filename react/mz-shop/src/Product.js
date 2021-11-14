8ea5541ed884cab54646e3e33b3a5e61f3e785f4
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2021-11-14 18:15:02 +0200

    React => useEffect Hook - Get Products

--- react/mz-shop/src/Product.js
+++ react/mz-shop/src/Product.js
@@ -1,9 +1,16 @@
 const Product = (props) => {
   return (
-    <div>
-      <h1>{props.title}</h1>
-      <h2>{props.description}</h2>
-      <h2>{props.price}</h2>
+    <div className="product">
+      <img src={props.image} className="product-avatar" alt={`product of ${props.title}`} />
+      <div className="product-details">
+        <h3 className="product-title">{props.title}</h3>
+        <div className="product-meta">
+          <p className="product-price">{props.price}</p>
+          <p className="product-category">{props.category}</p>
+        </div>
+        <p className="product-description">{props.description}</p>
+      </div>
+      <div className="product-remove">remove</div>
     </div>
   )
 }
--- react/mz-shop/src/ProductsList.js
+++ react/mz-shop/src/ProductsList.js
@@ -1,7 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import Product from './Product'
 
 export default function ProductsList() {
   const [query, setQuery] = useState('')
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    async function fetchMyProducts() {
+      const response = await axios.get(`${process.env.BASE_API_URL}/products`)
+      setProducts(response.data)
+    }
+    fetchMyProducts()
+
+    return () => {
+      console.info('Bye!')
+    }
+  }, [])
 
   return (
     <div className="list-products">
@@ -14,6 +30,28 @@ export default function ProductsList() {
           onChange={(event) => setQuery(event.target.value)}
         />
       </div>
+      <ol className="product-list">
+        {/* 1. showing it to validate */}
+        {/* <code>
+          <pre>{JSON.stringify(products, null, 2)}</pre>
+        </code> */}
+
+        {/* 2. it will work but it's not readable code as after a while you will have no idea what's in product */}
+        {/* {products && products.map((product) => <Product key={product.id} {...product} />)} */}
+
+        {/* 3. it's a little bit verbose but it's readable */}
+        {products &&
+          products.map((product) => (
+            <Product
+              key={product.id}
+              image={product.image}
+              title={product.title}
+              description={product.description}
+              price={product.price}
+              category={product.category}
+            />
+          ))}
+      </ol>
     </div>
   )
 }
